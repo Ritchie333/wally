3675c5545fbd6269f979d4db846b2f67e6f4a3e3
Ritchie Swann <ritchieswann@gmail.com>
2023-11-06 09:43:16 +0000

    friends don't let friends program by copy and paste

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-rm -rf hunch
 #sna2skool.py -H -c src/wally.ctl sources/wally.z80 > sources/wally.skool
 rm -rf wally
 skool2html.py -H -T dark -W src/:wally.WallyHtmlWriter src/wally.skool
