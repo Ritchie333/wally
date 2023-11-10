1ad88af92ed07d25903a786b11486869e96ee1b6
Ritchie Swann <ritchieswann@gmail.com>
2023-11-10 17:38:19 +0000

    minor fixes

--- buildasm.sh
+++ buildasm.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 rm -rf wally.asm
-skool2asm.py -H  -W src/:wally.WallyAsmWriter src/wally.skool > wally.asm
+skool2asm.py -H -S 31488 -E 64298  -W src/:wally.WallyAsmWriter src/wally.skool > wally.asm
